6379f4d3fe285fe5337207f7770362422fb44ed0
Tiago Fortunato Lino <tiago.fortunato@gmail.com>
2026-02-11 20:32:48 +1100

    fix: remove accidentally committed libs/openvino from tracking

--- libs/openvino/python/openvino/tools/__init__.pyi
+++
@@ -1,4 +0,0 @@
-# type: ignore
-from . import ovc
-from __future__ import annotations
-__all__: list[str] = ['ovc']
--- libs/openvino/python/openvino/tools/ovc/telemetry_params.pyi
+++
@@ -1,4 +0,0 @@
-# type: ignore
-from __future__ import annotations
-__all__: list[str] = ['telemetry_params']
-telemetry_params: dict = {'TID': 'G-W5E9RNLD4H'}
